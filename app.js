43a37216ff2866af7ce95b8a967d74c7c737c61b
aziz sahnoun <azizsahnoun5@gmail.com>
2021-07-19 11:39:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+// import mongoDB
+var mongoose = require("mongoose");
+var config = require("./database/mongodb");
+var cors = require("cors");
+
+var usersRouter = require("./routes/users");
+var indexRouter = require("./routes/index");
+
+// Delivery man Module's router
+
+//Package and Delivery Module's router
+
+//Customer & Entreprise Module's routers
+
+//Users Router
+
+var app = express();
+require("dotenv").config();
+const bodyParser = require("body-parser");
+
+// mongo config
+mongoose
+  .connect(config.mongo.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongo"))
+  .catch((err) => console.log(err));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+//Package & delivery Module's middlewares
+
+//Customer & Entreprise Module's middlewares
+
+//User middlewares
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+module.exports = app;
